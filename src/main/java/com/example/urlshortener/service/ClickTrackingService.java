8dec221e0cbc5e5771c544b8c0a28c76533850e9
Darshan <darshan2003.ise.rymec@gmail.com>
2026-09-26 22:12:01 +0530

    Update ClickTrackingService.java

--- src/main/java/com/example/urlshortener/service/ClickTrackingService.java
+++ src/main/java/com/example/urlshortener/service/ClickTrackingService.java
@@ -3,15 +3,18 @@ package com.example.urlshortener.service;
 import com.example.urlshortener.models.ClickEvent;
 import com.example.urlshortener.repository.ClickEventRepository;
 import com.example.urlshortener.repository.UrlMappingRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
 
 @Service
+@AllArgsConstructor
 public class ClickTrackingService {
     private UrlMappingRepository urlMappingRepository;
     private ClickEventRepository clickEventRepository;
+
     @Async
     public void trackClickAsync(Long urlMappingId){
         urlMappingRepository.incrementClickAsync(urlMappingId);
